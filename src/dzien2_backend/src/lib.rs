e58db8be7d555c27748a35ece2bb09e1084d4486
Pasek108 <pasartur108@gmail.com>
2024-06-25 19:53:03 +0200

    added blog posts backend and frontend

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -1,4 +1,25 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static POSTS: RefCell<Vec<String>> = RefCell::default();
+}
+
 #[ic_cdk::query]
 fn greet(name: String, last_name: String) -> String {
     format!("Hello, {} {}!", name, last_name)
+}
+
+#[ic_cdk::update]
+fn add_post(post: String) {
+    POSTS.with(|posts| {
+        posts.borrow_mut().push(post)
+    });
+}
+
+
+#[ic_cdk::query]
+fn read_posts() -> Vec<String> {
+    POSTS.with(|posts| {
+        posts.borrow().clone()
+    })
 }
---
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click="getPosts">Odśwież wpisy</button>
+        Siema blog
+        {{ posts }}
+        <input v-model="newPost" type="text">
+        <button @click="addPost">Dodaj wpis</button>
+    </div>
+</template>
+
+<script>
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
+
+export default {
+    data() {
+        return {
+            posts: [],
+            newPost: ""
+        }
+    }, 
+    methods: {
+        async addPost () {
+            await dzien2_backend.add_post(this.newPost)
+        },
+        async getPosts() {
+            this.posts = await dzien2_backend.read_posts()
+        }
+    },
+}
+</script>
