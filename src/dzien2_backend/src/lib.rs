de1c34e82c076c5808d3fc92991e12413ff3290a
Pasek108 <pasartur108@gmail.com>
2024-06-26 17:37:33 +0200

    added delete_post and edit_post

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -16,10 +16,26 @@ fn add_post(post: String) {
     });
 }
 
-
 #[ic_cdk::query]
 fn read_posts() -> Vec<String> {
     POSTS.with(|posts| {
         posts.borrow().clone()
     })
 }
+
+#[ic_cdk::update]
+fn delete_post(post_id: usize) {
+    POSTS.with(|posts| {
+        posts.borrow_mut().remove(post_id)
+    });
+}
+
+#[ic_cdk::update]
+fn edit_post(post_id: usize, new_post: String) {
+    POSTS.with(|posts| {
+        let mut binding = posts.borrow_mut();
+        let post = binding.get_mut(post_id);
+        let old_post = post.unwrap();
+        *old_post = new_post;
+    });
+} 
