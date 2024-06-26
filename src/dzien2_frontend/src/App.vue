8731de6991eb4bdd3404362979c4c3aaff6afa94
Pasek108 <pasartur108@gmail.com>
2024-06-26 20:01:48 +0200

    editing and deleting posts, frontend improvements

--- src/dzien2_frontend/src/App.vue
+++ src/dzien2_frontend/src/App.vue
@@ -1,32 +1,12 @@
 <script setup>
-import { ref } from 'vue';
-import { dzien2_backend } from 'declarations/dzien2_backend/index';
 import Blog from "./components/Blog.vue";
-let greeting = ref('');
-
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  const last_name = target.querySelector('#last_name').value;
-  await dzien2_backend.greet(name, last_name).then((response) => {
-    greeting.value = response;
-  });
-}
 </script>
 
 <template>
-  <main>
+  <main class="container m-auto">
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" placeholder="Name" />
-      <input id="last_name" alt="Last Name" type="text" placeholder="Last name" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
     <Blog />
   </main>
 </template>
--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,10 +1,25 @@
 <template>
     <div>
-        <button @click="getPosts">Odśwież wpisy</button>
-        Siema blog
-        {{ posts }}
-        <input v-model="newPost" type="text">
-        <button @click="addPost">Dodaj wpis</button>
+        <div class="w-100 mb-4 flex flex-row justify-between">
+            <h2 class="text-blue-600 inline-block">Wpisy na bloga</h2>
+            <button @click="getPosts" class="border border-gray-700 rounded-sm px-2 cursor-pointer">Odśwież wpisy</button>
+        </div>
+        
+        <div class="grid mx-6 gap-2 mb-4">
+            <div v-for="(post, index) in posts" class="flex flex-row justify-between drop-shadow-sm bg-stone-300 px-4 py-1">
+                <div>
+                    <span class=" inline-block w-20">{{ index }}. </span>
+                    <span>{{ post }}</span>
+                </div>
+                <div>
+                    <button v-on:click="editPost(index)" class="border border-gray-700 rounded-sm px-2 bg-blue-500 cursor-pointer">Edit</button>
+                    <button v-on:click="deletePost(index)" class="border border-gray-700 rounded-sm px-2 bg-red-500 cursor-pointer">Delete</button>
+                </div>
+            </div>
+        </div>
+        
+        <input v-model="newPost" type="text" class="mb-4 border border-gray-700 rounded-sm px-2">
+        <button @click="addPost" class="ml-2 mb-4 border border-gray-700 rounded-sm px-2 cursor-pointer">Dodaj wpis</button>
     </div>
 </template>
 
@@ -20,11 +35,21 @@ export default {
     }, 
     methods: {
         async addPost () {
-            await dzien2_backend.add_post(this.newPost)
+            await dzien2_backend.add_post(this.newPost).then(res => this.getPosts())
+        },
+        async editPost(id) {
+            let new_text = prompt("Edit post:")
+            await dzien2_backend.edit_post(id, new_text).then(res => this.getPosts())
+        },
+        async deletePost(id) {
+            await dzien2_backend.delete_post(id).then(res => this.getPosts())
         },
         async getPosts() {
             this.posts = await dzien2_backend.read_posts()
         }
     },
+    async mounted() {
+        this.getPosts()
+    }
 }
 </script>
